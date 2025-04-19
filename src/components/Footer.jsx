bd560b9ec345ad3d0cae137c506426a9e24f32ae
Subrat Kumar Parida <subratparida644@eamil.com>
2025-04-20 00:11:38 +0530

    updated the links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
         <div className="max-w-screen-2xl container mx-auto px-4 md:px-20">
             <div className="flex flex-col items-center justify-center size={24} ">
                 <div className="flex space-x-4">
-                    <BsFacebook size={24} />
-                    <BsTwitter size={24} />
-                    <BsInstagram size={24} />
-                    <BsLinkedin size={24} />
+                    <a href="https://www.facebook.com/subratkumarparida.dinesh" target='_blank'><BsFacebook size={24} /></a>
+                    <a href="https://twitter.com/SubratK08?t=fYYZVt-pQLcFpMluPX-0uw&s=09" target='_blank'><BsTwitter size={24} /></a>
+                    <a href="https://www.instagram.com/subratkumar_05/"><BsInstagram size={24} /></a>
+                    <a href="https://www.linkedin.com/in/subrat-kumar-parida-871553237"><BsLinkedin size={24} /></a>
                 </div>
                 <div className="mt-8 border-t border-gray-700 pt-8 flex flex-col items-center">
                     <p>
