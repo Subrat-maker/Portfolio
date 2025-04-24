4798d87b80768d68edd6509bcda4bfcefeece19f
Subrat Kumar Parida <subratparida644@eamil.com>
2025-04-25 00:29:28 +0530

    resume and cards updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,46 +7,58 @@ import nodejs from "../../public/node.png"
 import sql from "../../public/sql.png"
 import git from "../../public/git.png"
 import vediting from "../../public/vediting.png"
+import dotnet from "../../public/dotnet.png"
+import csharp from "../../public/csharp.png"
 function Experience() {
   const cardItem=[
     {
       id:1,
+      logo:dotnet,
+      name:"DOT NET"
+    },
+    {
+      id:2,
+      logo:csharp,
+      name:"C#"
+    },
+    {
+      id:3,
       logo:html,
       name:"HTML"
     },
     {
-      id:2,
+      id:4,
       logo:css,
       name:"CSS"
     },
     {
-      id:3,
+      id:5,
       logo:javascript,
       name:"Javascript"
     },
     {
-      id:4,
+      id:6,
       logo:reactjs,
       name:"ReactJs"
     },
     {
-      id:5,
+      id:7,
       logo:nodejs,
       name:"NodeJs"
     },
    
     {
-      id:6,
+      id:8,
       logo:sql,
       name:"SQL"
     },
     {
-      id:7,
+      id:9,
       logo:git,
       name:"Git/GitHub"
     },
     {
-      id:8,
+      id:10,
       logo:vediting,
       name:"Video Editing"
     }
